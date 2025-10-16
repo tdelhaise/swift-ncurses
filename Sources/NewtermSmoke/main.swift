f98d330abfc8701f1cf0f846631e6c5b74af3f6e
Thierry DELHAISE <thierry.delhaise@gmail.com>
2025-10-16 11:03:56 +0200

    Update main.swift

--- Sources/NewtermSmoke/main.swift
+++ Sources/NewtermSmoke/main.swift
@@ -5,20 +5,25 @@ import Darwin
 import Glibc
 #endif
 
-@MainActor
-private func run() {
-  setenv("TERM", "xterm-256color", 1)
-  guard let screen = newterm(nil, stdout, stdin) else {
-    _ = fputs("newterm() failed\n", stderr)
-    exit(1)
-  }
-  defer { endwin(); delscreen(screen) }
-  print("OK: newterm/init+cleanup")
-}
-
 @main
 struct NewtermSmokeMain {
   static func main() {
-    run()
+    // Évite l’accès aux globals C non sendable (stdin/stdout/stderr)
+    setenv("TERM", "xterm-256color", 1)
+
+    let inFile  = fdopen(0, "r")
+    let outFile = fdopen(1, "w")
+    let errFile = fdopen(2, "w")
+
+    guard let screen = newterm(nil, outFile, inFile) else {
+      _ = fputs("newterm() failed\n", errFile)
+      exit(1)
+    }
+    defer {
+      endwin()
+      delscreen(screen)
+      fflush(outFile)
+    }
+    print("OK: newterm/init+cleanup")
   }
 }
