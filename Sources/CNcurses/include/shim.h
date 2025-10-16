d0a5ad9e8abbaf9546b1b6580d3d6b157b253da3
Thierry DELHAISE <thierry.delhaise@gmail.com>
2025-10-16 11:32:01 +0200

    Update shim.h

--- Sources/CNcurses/include/shim.h
+++ Sources/CNcurses/include/shim.h
@@ -1,7 +1,16 @@
 #pragma once
 
-#include <ncursesw/curses.h>
-#include <ncursesw/panel.h>
-#include <ncursesw/menu.h>
-#include <ncursesw/form.h>
+#if defined(__APPLE__)
+  // Sur macOS, utiliser les headers système du SDK pour éviter les conflits
+  #include <curses.h>
+  #include <panel.h>
+  #include <menu.h>
+  #include <form.h>
+#else
+  // Sur Linux, utiliser les headers vendorisés ncursesw/*
+  #include <ncursesw/curses.h>
+  #include <ncursesw/panel.h>
+  #include <ncursesw/menu.h>
+  #include <ncursesw/form.h>
+#endif
 
